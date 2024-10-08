7a829259a953e6f1872a3f274670a07e4516ec1d
Raghav Jindal <raghavjindal0212@gmail.com>
2024-10-09 02:57:33 +0530

    treeshaking

--- app/admin/actions.ts
+++ app/admin/actions.ts
@@ -1,15 +1,1 @@
-"use server";
-
-import {
-  getAllCollections,
-  getCollectionsByArtistId,
-} from "@/lib/supabaseClient";
-
-export async function loginAdmin(password: string) {
-  if (password === process.env.ADMIN_PASSWORD) {
-    const fetchedCollections = await getAllCollections();
-    return { success: true, collections: fetchedCollections };
-  } else {
-    return { success: false, collections: [] };
-  }
-}
+"Email daryl@irls.xyz to view this code !"
